068c6d97052a70b6a97643d814ade8c363531378
Mao Puerta Ospina <mpuertao@gmail.com>
2020-07-23 01:48:32 -0500

    correccion contractest

--- creditcheckservice/src/test/java/com/mpuertao/creditcheckservice/BaseContractTest.java
+++ creditcheckservice/src/test/java/com/mpuertao/creditcheckservice/BaseContractTest.java
@@ -5,17 +5,17 @@ import com.mpuertao.creditcheckservice.gateway.CreditCheckResponse;
 import com.mpuertao.creditcheckservice.service.CreditCheckService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
+import org.mockito.Mockito;
 
 import static com.mpuertao.creditcheckservice.gateway.CreditCheckResponse.Score.HIGH;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class BaseContractTest {
 
     @Before
     public void setUp() {
         final CreditCheckService mock = mock(CreditCheckService.class);
-        when(mock.doCreditCheck(1234)).thenReturn(new CreditCheckResponse(HIGH));
+        Mockito.when(mock.doCreditCheck(1234)).thenReturn(new CreditCheckResponse(HIGH));
         RestAssuredMockMvc.standaloneSetup(new CreditCheckController(mock));
     }
 }
